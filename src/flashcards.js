d4908cd669acdadefae126dfc312b9447573d7c9
rivalcoba <ivan.rivalcoba@gmail.com>
2026-04-16 19:42:55 -0600

    ✨Feat: Add new flashcards and enhance styling for improved layout and responsiveness

--- src/flashcards.js
+++ src/flashcards.js
@@ -1,80 +1,455 @@
 // Arreglo de objetos con las palabras
 const flashcardsData = [
-    {
-        word: "animals",
-        phonetic: "/ˈæn.ɪ.məlz/",
-        url: "https://img.freepik.com/free-vector/set-wild-animals_1308-29055.jpg"
-    },
-    {
-        word: "penguin",
-        phonetic: "/ˈpeŋ.ɡwɪn/",
-        url: "https://images.unsplash.com/photo-1551986782-d0169b3f8fa7?w=400"
-    },
-    {
-        word: "hippo",
-        phonetic: "/ˈhɪp.oʊ/",
-        url: "https://i.pinimg.com/236x/7f/43/1b/7f431b01adc228281387f7548143da0c.jpg"
-    },
-    {
-        word: "tiger",
-        phonetic: "/ˈtaɪ.ɡɚ/",
-        url: "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?w=400"
-    },
-    {
-        word: "elephant",
-        phonetic: "/ˈel.ɪ.fənt/",
-        url: "https://images.unsplash.com/photo-1564760055775-d63b17a55c44?w=400"
-    },
-    {
-        word: "banks",
-        phonetic: "/bæŋks/",
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8UlVzhTogJaR6aPKSkg8dvkpGC1Dj0b_OQ&s"
-    },
-    {
-        word: "giraffe",
-        phonetic: "/dʒɪˈræf/",
-        url: "https://images.unsplash.com/photo-1547721064-da6cfb341d50?w=400"
-    },
-    {
-        word: "parrot",
-        phonetic: "/ˈper.ət/",
-        url: "https://images.unsplash.com/photo-1552728089-57bdde30beb3?w=400"
-    },
-    {
-        word: "crocodile",
-        phonetic: "/ˈkrɑː.kə.daɪl/",
-        url: "https://img.freepik.com/free-vector/cute-crocodile-waving-hand-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated_138676-6015.jpg?semt=ais_user_personalization&w=740&q=80"
-    },
-    {
-        word: "rhino",
-        phonetic: "/ˈraɪ.noʊ/",
-        url: "https://thumbs.dreamstime.com/b/big-white-rhino-16802916.jpg"
-    },
-    {
-        word: "zebra",
-        phonetic: "/ˈziː.brə/",
-        url: "https://images.unsplash.com/photo-1501706362039-c06b2d715385?w=400"
-    },
-    {
-        word: "jump",
-        phonetic: "/dʒʌmp/",
-        url: "https://static.vecteezy.com/system/resources/thumbnails/048/933/901/small/box-jumps-fitness-exercise-leg-color-icon-illustration-vector.jpg"
-    },
-    {
-        word: "eat",
-        phonetic: "/iːt/",
-        url: "https://img.freepik.com/free-vector/kid-eating-illustration_24908-81084.jpg?semt=ais_user_personalization&w=740&q=80"
-    },
-    {
-        word: "drink",
-        phonetic: "/drɪŋk/",
-        url: "https://img.freepik.com/premium-vector/man-drinking-milk-vector-illustration_1062857-1974.jpg?semt=ais_user_personalization&w=740&q=80"
-    },
-    {
-        word: "swim",
-        phonetic: "/swɪm/",
-        url: "https://thumbs.dreamstime.com/b/illustration-male-swimmer-pool-performing-front-crawl-wears-blue-swim-trunks-goggles-visible-water-flow-411772138.jpg"
-    }
+  {
+    word: "Amazon River",
+    phonetic: "/ˈæm.ə.zən ˈrɪv.ɚ/",
+    url: "#"
+  },
+  {
+    word: "apes",
+    phonetic: "/eɪps/",
+    url: "#"
+  },
+  {
+    word: "astronomer",
+    phonetic: "/əˈstrɑː.nə.mɚ/",
+    url: "#"
+  },
+  {
+    word: "attention",
+    phonetic: "/əˈtɛn.ʃən/",
+    url: "#"
+  },
+  {
+    word: "bacteria",
+    phonetic: "/bækˈtɪr.i.ə/",
+    url: "#"
+  },
+  {
+    word: "banks",
+    phonetic: "/bæŋks/",
+    url: "#"
+  },
+  {
+    word: "blast",
+    phonetic: "/blæst/",
+    url: "#"
+  },
+  {
+    word: "blushed",
+    phonetic: "/blʌʃt/",
+    url: "#"
+  },
+  {
+    word: "boarded",
+    phonetic: "/ˈbɔːr.dɚd/",
+    url: "#"
+  },
+  {
+    word: "bright",
+    phonetic: "/braɪt/",
+    url: "#"
+  },
+  {
+    word: "British",
+    phonetic: "/ˈbrɪt.ɪʃ/",
+    url: "#"
+  },
+  {
+    word: "bug spray",
+    phonetic: "/ˈbʌɡ ˌspreɪ/",
+    url: "#"
+  },
+  {
+    word: "century",
+    phonetic: "/ˈsɛn.tʃɚ.i/",
+    url: "#"
+  },
+  {
+    word: "clothes",
+    phonetic: "/kloʊðz/",
+    url: "#"
+  },
+  {
+    word: "confident",
+    phonetic: "/ˈkɑːn.fə.dənt/",
+    url: "#"
+  },
+  {
+    word: "cool down",
+    phonetic: "/ˌkuːl ˈdaʊn/",
+    url: "#"
+  },
+  {
+    word: "creek",
+    phonetic: "/kriːk/",
+    url: "#"
+  },
+  {
+    word: "dam",
+    phonetic: "/dæm/",
+    url: "#"
+  },
+  {
+    word: "dizzy",
+    phonetic: "/ˈdɪz.i/",
+    url: "#"
+  },
+  {
+    word: "dots and dashes",
+    phonetic: "/ˌdɑːts ən ˈdæʃ.ɪz/",
+    url: "#"
+  },
+  {
+    word: "drink up",
+    phonetic: "/ˌdrɪŋk ˈʌp/",
+    url: "#"
+  },
+  {
+    word: "drop",
+    phonetic: "/drɑːp/",
+    url: "#"
+  },
+  {
+    word: "earthquakes",
+    phonetic: "/ˈɝːθ.kweɪks/",
+    url: "#"
+  },
+  {
+    word: "embarrassed",
+    phonetic: "/ɪmˈbɛr.əst/",
+    url: "#"
+  },
+  {
+    word: "English",
+    phonetic: "/ˈɪŋ.ɡlɪʃ/",
+    url: "#"
+  },
+  {
+    word: "enough",
+    phonetic: "/ɪˈnʌf/",
+    url: "#"
+  },
+  {
+    word: "entertaining",
+    phonetic: "/ˌɛn.tɚˈteɪ.nɪŋ/",
+    url: "#"
+  },
+  {
+    word: "Europe",
+    phonetic: "/ˈjʊr.əp/",
+    url: "#"
+  },
+  {
+    word: "exclaims",
+    phonetic: "/ɪkˈskleɪmz/",
+    url: "#"
+  },
+  {
+    word: "experiment",
+    phonetic: "/ɪkˈspɛr.ə.mənt/",
+    url: "#"
+  },
+  {
+    word: "faucet",
+    phonetic: "/ˈfɑː.sɪt/",
+    url: "#"
+  },
+  {
+    word: "flashlight",
+    phonetic: "/ˈflæʃ.laɪt/",
+    url: "#"
+  },
+  {
+    word: "Flush",
+    phonetic: "/flʌʃ/",
+    url: "#"
+  },
+  {
+    word: "foreground",
+    phonetic: "/ˈfɔːr.ɡraʊnd/",
+    url: "#"
+  },
+  {
+    word: "forgive",
+    phonetic: "/fɚˈɡɪv/",
+    url: "#"
+  },
+  {
+    word: "forty-one",
+    phonetic: "/ˌfɔːr.tiˈwʌn/",
+    url: "#"
+  },
+  {
+    word: "forty-two",
+    phonetic: "/ˌfɔːr.tiˈtuː/",
+    url: "#"
+  },
+  {
+    word: "French",
+    phonetic: "/frɛntʃ/",
+    url: "#"
+  },
+  {
+    word: "Friday",
+    phonetic: "/ˈfraɪ.deɪ/",
+    url: "#"
+  },
+  {
+    word: "galaxy",
+    phonetic: "/ˈɡæl.ək.si/",
+    url: "#"
+  },
+  {
+    word: "goods",
+    phonetic: "/ɡʊdz/",
+    url: "#"
+  },
+  {
+    word: "grateful",
+    phonetic: "/ˈɡreɪt.fəl/",
+    url: "#"
+  },
+  {
+    word: "grumbled",
+    phonetic: "/ˈɡrʌm.bəld/",
+    url: "#"
+  },
+  {
+    word: "hammock",
+    phonetic: "/ˈhæm.ək/",
+    url: "#"
+  },
+  {
+    word: "healthy",
+    phonetic: "/ˈhɛl.θi/",
+    url: "#"
+  },
+  {
+    word: "Hose",
+    phonetic: "/hoʊz/",
+    url: "#"
+  },
+  {
+    word: "iron",
+    phonetic: "/ˈaɪ.ɚn/",
+    url: "#"
+  },
+  {
+    word: "irrigate",
+    phonetic: "/ˈɪr.ə.ɡeɪt/",
+    url: "#"
+  },
+  {
+    word: "January",
+    phonetic: "/ˈdʒæn.juˌɛr.i/",
+    url: "#"
+  },
+  {
+    word: "Japanese",
+    phonetic: "/ˌdʒæp.əˈniːz/",
+    url: "#"
+  },
+  {
+    word: "jewelry",
+    phonetic: "/ˈdʒuː.əl.ri/",
+    url: "#"
+  },
+  {
+    word: "landscape",
+    phonetic: "/ˈlænd.skeɪp/",
+    url: "#"
+  },
+  {
+    word: "laugh at",
+    phonetic: "/ˈlæf æt/",
+    url: "#"
+  },
+  {
+    word: "launch",
+    phonetic: "/lɔːntʃ/",
+    url: "#"
+  },
+  {
+    word: "length",
+    phonetic: "/lɛŋkθ/",
+    url: "#"
+  },
+  {
+    word: "mechanical",
+    phonetic: "/məˈkæn.ɪ.kəl/",
+    url: "#"
+  },
+  {
+    word: "melt",
+    phonetic: "/mɛlt/",
+    url: "#"
+  },
+  {
+    word: "Monday",
+    phonetic: "/ˈmʌn.deɪ/",
+    url: "#"
+  },
+  {
+    word: "network",
+    phonetic: "/ˈnɛt.wɝːk/",
+    url: "#"
+  },
+  {
+    word: "North Pole",
+    phonetic: "/ˌnɔːrθ ˈpoʊl/",
+    url: "#"
+  },
+  {
+    word: "November",
+    phonetic: "/noʊˈvɛm.bɚ/",
+    url: "#"
+  },
+  {
+    word: "October",
+    phonetic: "/ɑːkˈtoʊ.bɚ/",
+    url: "#"
+  },
+  {
+    word: "parachuted",
+    phonetic: "/ˈpær.əˌʃuː.tɪd/",
+    url: "#"
+  },
+  {
+    word: "portrait",
+    phonetic: "/ˈpɔːr.treɪt/",
+    url: "#"
+  },
+  {
+    word: "pottery",
+    phonetic: "/ˈpɑː.t̬ɚ.i/",
+    url: "#"
+  },
+  {
+    word: "powerful",
+    phonetic: "/ˈpaʊ.ɚ.fəl/",
+    url: "#"
+  },
+  {
+    word: "pressed",
+    phonetic: "/prɛst/",
+    url: "#"
+  },
+  {
+    word: "pump",
+    phonetic: "/pʌmp/",
+    url: "#"
+  },
+  {
+    word: "reflection",
+    phonetic: "/rɪˈflɛk.ʃən/",
+    url: "#"
+  },
+  {
+    word: "relaxing",
+    phonetic: "/rɪˈlæk.sɪŋ/",
+    url: "#"
+  },
+  {
+    word: "reservoir",
+    phonetic: "/ˈrɛz.ɚ.vwɑːr/",
+    url: "#"
+  },
+  {
+    word: "shadow",
+    phonetic: "/ˈʃæd.oʊ/",
+    url: "#"
+  },
+  {
+    word: "shelter",
+    phonetic: "/ˈʃɛl.tɚ/",
+    url: "#"
+  },
+  {
+    word: "sketch",
+    phonetic: "/skɛtʃ/",
+    url: "#"
+  },
+  {
+    word: "spare",
+    phonetic: "/spɛr/",
+    url: "#"
+  },
+  {
+    word: "steam",
+    phonetic: "/stiːm/",
+    url: "#"
+  },
+  {
+    word: "strength",
+    phonetic: "/strɛŋkθ/",
+    url: "#"
+  },
+  {
+    word: "sunscreen",
+    phonetic: "/ˈsʌnˌskriːn/",
+    url: "#"
+  },
+  {
+    word: "thermometer",
+    phonetic: "/θɚˈmɑː.mə.t̬ɚ/",
+    url: "#"
+  },
+  {
+    word: "thirty-four",
+    phonetic: "/ˌθɝː.tiˈfɔːr/",
+    url: "#"
+  },
+  {
+    word: "timeline",
+    phonetic: "/ˈtaɪm.laɪn/",
+    url: "#"
+  },
+  {
+    word: "toddlers",
+    phonetic: "/ˈtɑːd.lɚz/",
+    url: "#"
+  },
+  {
+    word: "trading",
+    phonetic: "/ˈtreɪ.dɪŋ/",
+    url: "#"
+  },
+  {
+    word: "troubles",
+    phonetic: "/ˈtrʌb.əlz/",
+    url: "#"
+  },
+  {
+    word: "underground",
+    phonetic: "/ˌʌn.dɚˈɡraʊnd/",
+    url: "#"
+  },
+  {
+    word: "vegetables",
+    phonetic: "/ˈvɛdʒ.tə.bəlz/",
+    url: "#"
+  },
+  {
+    word: "Viking",
+    phonetic: "/ˈvaɪ.kɪŋ/",
+    url: "#"
+  },
+  {
+    word: "water plant",
+    phonetic: "/ˈwɔː.t̬ɚ plænt/",
+    url: "#"
+  },
+  {
+    word: "weave",
+    phonetic: "/wiːv/",
+    url: "#"
+  },
+  {
+    word: "width",
+    phonetic: "/wɪdθ/",
+    url: "#"
+  }
 ];
 
 // Variable para el nombre (modificable desde el script)
